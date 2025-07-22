68c4dff72341d34f35402441f73f890acd359dc6
bhaskrr <74664799+bhaskrr@users.noreply.github.com>
2025-07-22 20:17:45 +0530

    added genre to generate story function and prompt

--- src/lib/ai.ts
+++ src/lib/ai.ts
@@ -1,14 +1,14 @@
 import { groq } from "@ai-sdk/groq";
 import { generateText } from "ai";
 
-export async function generateStoryFromNotes(notes: string, tone: string, length: string, setting: string, goal: string, theme: string, ): Promise<string> {
+export async function generateStoryFromNotes(notes: string, tone: string, length: string, setting: string, goal: string, theme: string, genre: string, ): Promise<string> {
 
     const result = await generateText({
         model: groq('llama-3.3-70b-versatile'),
         system:
             'You are a creative and professional storyteller. ' +
             'You transform raw notes into engaging and memorable narratives that are easy to read and remember.',
-        prompt: `Turn the following notes into a ${length}-length story in a ${tone}-tone in simple language. Set the story in a ${setting} setting.
+        prompt: `Turn the following notes into a ${length}-length story in a ${tone}-tone and ${genre}-genre in simple language. Set the story in a ${setting} setting.
                 Make the story suitable for ${goal}. Focus on helping the reader understand the concept clearly through narrative in a ${theme} theme.
                 Keep it engaging and vivid:\n\n${notes}`,
     });
